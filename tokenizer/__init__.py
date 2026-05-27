2acfb0aa67284399f7db12935a8f53b7b1537c8b
Hsuant <296946361@qq.com>
2026-05-27 21:22:23 +0800

    chore: update __init__.py to expose core tokenizer components

--- tokenizer/__init__.py
+++ tokenizer/__init__.py
@@ -3,7 +3,6 @@ from tokenizer.decoder import DecoderFactory
 from tokenizer.pre_tokenizer import PreTokenizerFactory
 from tokenizer.post_processor import PostProcessorFactory
 from tokenizer.tokenizer import TaiChuTokenizerTrainer
-from tokenizer.trainer import TrainerFactory
 
 __all__ = [
     'CorpusIterator',
@@ -11,5 +10,4 @@ __all__ = [
     'PreTokenizerFactory',
     'PostProcessorFactory',
     'TaiChuTokenizerTrainer',
-    'TrainerFactory',
 ]
