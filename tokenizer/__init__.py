5878dac81a33799484e06a4f5e3d7544ba505743
Hsuant <296946361@qq.com>
2026-05-31 14:30:38 +0800

    feat(tokenizer): expose new tokenizer components in package init
    - Export CorpusIterator, DecoderFactory, NormalizerFactory,
      PreTokenizerFactory, TaiChuTokenizerTrainer for external use.

--- tokenizer/__init__.py
+++ tokenizer/__init__.py
@@ -1,13 +1,13 @@
 from tokenizer.corpus import CorpusIterator
 from tokenizer.decoder import DecoderFactory
+from tokenizer.normalizer import NormalizerFactory
 from tokenizer.pre_tokenizer import PreTokenizerFactory
-from tokenizer.post_processor import PostProcessorFactory
 from tokenizer.tokenizer import TaiChuTokenizerTrainer
 
 __all__ = [
     'CorpusIterator',
     'DecoderFactory',
+    'NormalizerFactory',
     'PreTokenizerFactory',
-    'PostProcessorFactory',
     'TaiChuTokenizerTrainer',
 ]
